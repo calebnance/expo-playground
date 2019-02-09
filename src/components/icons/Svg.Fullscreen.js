d5d1f4ba1b261004692c65a4fe1a32897acfaf1a
Caleb Nance <calebnance@gmail.com>
2019-02-09 13:23:42 -0500

    Video example SVG icons and asset

---
+++ src/components/icons/Svg.Fullscreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Svg } from 'expo';
+import PropTypes from 'prop-types';
+import { colors } from '../../api/constants';
+
+const SvgFullscreen = props => {
+  const { active, size } = props;
+
+  return (
+    <Svg height={size} width={size} viewBox="0 0 24 24">
+      <Svg.Path
+        d="M8 20H5c-.6 0-1-.4-1-1v-3c0-.6-.4-1-1-1s-1 .4-1 1v3c0 1.7 1.3 3 3 3h3c.6 0 1-.4 1-1s-.4-1-1-1zM8 2H5C3.3 2 2 3.3 2 5v3c0 .6.4 1 1 1s1-.4 1-1V5c0-.6.4-1 1-1h3c.6 0 1-.4 1-1s-.4-1-1-1zM19 2h-3c-.6 0-1 .4-1 1s.4 1 1 1h3c.6 0 1 .4 1 1v3c0 .6.4 1 1 1s1-.4 1-1V5c0-1.7-1.3-3-3-3zM21 15c-.6 0-1 .4-1 1v3c0 .6-.4 1-1 1h-3c-.6 0-1 .4-1 1s.4 1 1 1h3c1.7 0 3-1.3 3-3v-3c0-.6-.4-1-1-1z"
+        fill={active ? colors.brandPrimary : colors.brandSecondary}
+      />
+    </Svg>
+  );
+};
+
+SvgFullscreen.defaultProps = {
+  active: true,
+  size: 24
+};
+
+SvgFullscreen.propTypes = {
+  // optional
+  active: PropTypes.bool,
+  size: PropTypes.number
+};
+
+export default SvgFullscreen;
