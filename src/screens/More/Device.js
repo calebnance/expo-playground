2ded2589f6bb7814506bc3f68876bedf3f78bb0b
calebnance <calebnance@gmail.com>
2021-06-29 23:59:27 -0400

    more device information

--- src/screens/More/Device.js
+++ src/screens/More/Device.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 import Constants from 'expo-constants';
-import { device, gStyle } from '../../constants';
+import * as Device from 'expo-device';
+import { device, func, gStyle } from '../../constants';
 
 // components
 import LineItem from '../../components/LineItem';
 
-const Device = () => (
-  <ScrollView style={gStyle.container}>
-    <LineItem label="Device" text={device.iOS ? 'Apple' : 'Android'} />
-    <LineItem
-      label="Running On"
-      text={Constants.isDevice ? 'Physcial Device' : 'Simulator/Emulator'}
-    />
-    <LineItem label="Device Name" text={Constants.deviceName} />
-    <LineItem label="Device Year" text={Constants.deviceYearClass} />
-    <LineItem
-      label="Device Dimensions"
-      text={`${device.height}h by ${device.width}w`}
-    />
-  </ScrollView>
-);
+const DeviceScreen = () => {
+  const formattedMemory = func.formatBytes(Device.totalMemory);
 
-export default Device;
+  return (
+    <ScrollView style={gStyle.container}>
+      <LineItem
+        label="Running On"
+        text={Constants.isDevice ? 'Physcial Device' : 'Simulator/Emulator'}
+      />
+      <LineItem label="Device" text={device.iOS ? 'Apple' : 'Android'} />
+      <LineItem label="Device Model" text={Device.modelName} />
+      <LineItem label="Device OS Version" text={Device.osVersion} />
+      <LineItem label="Device Year" text={Constants.deviceYearClass} />
+      <LineItem label="Device Name" text={Constants.deviceName} />
+      <LineItem
+        label="Device Dimensions"
+        text={`${device.height}h by ${device.width}w`}
+      />
+      <LineItem label="Device Memory" text={formattedMemory} />
+    </ScrollView>
+  );
+};
+
+export default DeviceScreen;
